acc9c8f67790aa2564d7034e100a731e3891c9a3
Uğur Kap <ugurcan.kap@gmail.com>
2020-01-21 22:14:33 +0300

    Installed express and created css, js and html files.
    Installed express and created css, js and html files.

--- readme.md
+++ readme.md
@@ -3,6 +3,6 @@
 # TODO List
  - Build mapbox api request and get location information *** Done => Simple Boston location data
  - Build darksky api request and get weather information about location *** Done => Simple Los Angeles weather forecast data
- - Install express.js and build server and view
+ - Install express.js and build server and view *** Done => Created Index.html, style.css and app.js
  - Combine services
  - Show the information and enjoy
--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
+var express = require('express')
+var path = require('path')
 var geocode = require('../src/services/geocode')
 var forecast = require('../src/services/forecast')
 
-geocode((error, response) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(response)
-    }
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index')
 })
 
-forecast((error, response) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(response)
-    }
+// geocode((error, response) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log(response)
+//     }
+// })
+
+// forecast((error, response) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log(response)
+//     }
+// })
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
